b058cd6dcd1d44b2fdb7c767951c8587f07d0c8e
Jesus Alberto Ortega Hernández <jesus.ortega103@alu.ulpgc.es>
2023-12-30 19:47:48 +0000

    Creation of class TvsFileCustomerLoader.

--- src/software/ulpgc/kata2/TsvFileCustomerLoader.java
+++ src/software/ulpgc/kata2/TsvFileCustomerLoader.java
@@ -47,4 +47,5 @@ public class TsvFileCustomerLoader implements CustomerLoader {
     private Customer toCustomer(String[] fields) {
         return new Customer(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7], fields[8], fields[9], fields[10]);
     }
+
 }
